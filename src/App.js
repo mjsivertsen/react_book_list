e445db8c9bec42e8f469cd2a02588bfcbefd39be
Michaela Sivertsen <mjsivertsen@msn.com>
2021-08-31 15:18:56 -0600

    init, got renderr working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { REGEX_BACKSLASH } from "picomatch/lib/constants";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState([]);
+ 
+  useEffect(() => {
+    console.log("useEffect called");
+    getBooks();
+  }, []);
+  
+  const getBooks = async () => {
+    try {
+      let res = await axios.get("https://fakerapi.it/api/v1/books?_quantity=5");
+    console.log(res.data.data);
+    setBooks(res.data.data);
+  } catch (err) {
+    alert("error occured getting books");
+    console.log(err);
+  }
+  };
+
+  const renderBooks = () => {
+    if(books.length == 0) {
+      return <p> No Books To Show </p>;
+    }
+
+    return books.map((book) => {
+      return (
+        <div style={styles.container} key={book.isbn}>
+          <em><h1>{book.title}</h1></em>
+          <h2>{`By ${book.author}`}</h2>
+          <img style={{height:"200px"}} src={book.image} />
+          <p style={{size:"3px"}}>{book.description}</p>
+          <h5>{`Genre: ${book.genre}`}</h5>
+          <h6> {`Publisher: ${book.publisher}`} </h6>
+          <h6> {`Published: ${book.published}`}</h6>
+          <h6>{`ISBN: ${book.isbn}`}</h6>
+        </div>
+      )
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> 
+     <u><h1>REACT BOOK LIST</h1></u>
+      
+      {renderBooks()}
+      </div>
+    
   );
+};
+
+const styles = {
+container: {
+  maxWidth: "800px",
+  padding: "22px",
+  margin: "22px",
+  boxShadow: "rgba(0, 0, 0, .6) 0px 6px 18px",
+}
+
 }
 
 export default App;
